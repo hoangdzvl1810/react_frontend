05b1490a06f6e7714861b6d96ab5b759490cbf68
DaoDuyHieu <duyh2934@gmail.com>
2026-07-15 17:05:32 +0700

    sửa cate

--- src/pages/AdminCategories.jsx
+++ src/pages/AdminCategories.jsx
@@ -38,8 +38,6 @@ export default function AdminCategories() {
 
       if (sort === "productAsc") return countA - countB;
       if (sort === "productDesc") return countB - countA;
-      if (sort === "nameAsc") return a.name.localeCompare(b.name);
-      if (sort === "nameDesc") return b.name.localeCompare(a.name);
 
       return a.id - b.id;
     });
@@ -185,8 +183,6 @@ export default function AdminCategories() {
             <option value="">Sắp xếp mặc định</option>
             <option value="productDesc">Nhiều mặt hàng nhất</option>
             <option value="productAsc">Ít mặt hàng nhất</option>
-            <option value="nameAsc">Tên A-Z</option>
-            <option value="nameDesc">Tên Z-A</option>
           </select>
 
           <button
